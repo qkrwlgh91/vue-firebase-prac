ff0eb127f422af8589a70a5211949eb203ca39f7
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-02-22 16:34:05 +0900

    side bar with vuetify

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,27 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>page title</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon to="/about">
+      <v-btn icon to="/about" >
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+    </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
@@ -26,7 +40,12 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 
 }
 </script>
