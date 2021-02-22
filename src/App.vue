82933ecaa51bc4a582e7337d77d543daea1083a0
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-02-22 16:20:33 +0900

    vuetify footer

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,11 @@
     <v-content>
       <router-view />
     </v-content>
+    <!-- v-footer에서 app이 있을때는 바닦에 항상 보이고 app이 없으면 스크롤 가장 밑에 보인다 -->
+    <v-footer app color="primary" dark absolute>
+      <v-spacer />
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
   </v-app>
 </template>
 
