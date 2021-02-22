9c688e5a52a0498eed696436927c889dd8dd734f
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-02-22 16:10:06 +0900

    vuetify app-bar && v-content

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,27 @@
 <template>
   <v-app>
+    <!-- v-app-bar 에서 app을 넣어주지 않으면 vuetify가 어디에 달라 붙어야할지 감지를 못함... -->
     <v-app-bar
       app
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>page title</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-content>
+      <router-view />
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
+  name: 'App'
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
 }
 </script>
