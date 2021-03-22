5bbf392f55dd7fc9192a4c660a6ae35d243f33a3
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-03-23 00:36:32 +0900

    vue-excel-export set up

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import ElementUI from 'element-ui'
+import excel from 'vue-excel-export'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(excel)
 
 new Vue({
   router,
